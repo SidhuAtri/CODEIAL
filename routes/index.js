83baa1fa9112221cab1b48b1ea8fe1f546592ba2
sidharth atri <sidharthatri@gmail.com>
2019-08-23 15:42:42 +0530

    install ejs and setup view engine

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const homecontroller = require('../controllers/home_controller');
 
 router.get('/',homecontroller.home);
+router.use('/users',require('./users'));
+
+//for any further routes excess from here
+//router.use('/routername',require('./routerfile'));
+
 console.log("Router Loaded");
 
 module.exports = router;

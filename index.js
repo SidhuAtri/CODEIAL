c356e018486c1f17dc4275d2056e5c5b923af4fa
sidharth atri <sidharthatri@gmail.com>
2019-08-28 22:17:48 +0530

    User Signning up

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+const cookieParser = require('cookie-parser');
 
-// app.use(express.static('./assets'));
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
-
-app.use(express.static('./assets'));
 
 //use express router
 app.use('/',require('./routes'));

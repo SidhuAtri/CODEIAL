cff2a2fbed6b0fc2bda3820ff337b5f51b902a85
sidharth atri <sidharthatri@gmail.com>
2019-08-30 00:25:11 +0530

    Sign in user authenticaation

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,21 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{title:"User Profile"});
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:"User Profile",
+                    user: user
+                })
+            }else{
+                return res.redirect('/users/sign-in');
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+    // return res.render('user_profile',{title:"User Profile"});
 }
 
 //render the sign up page 
@@ -47,5 +61,29 @@ module.exports.create = function(req,res)
 //sign in and create the session for the user
 module.exports.createSession = function(req,res)
 {
-    //todo later
+   //find the user
+   User.findOne({email: req.body.email},function(err,user){
+       if(err){
+        console.log('Error in finding user in signin');
+        return;       
+       }
+
+       //handle user found
+
+       if(user){
+
+        //handle password which doesn't match
+
+        if(user.password != req.body.password){
+            return res.redirect('back');
+        }
+
+        //handle session creation
+        res.cookie('user_id',user.id);
+        return res.redirect('/users/profile');
+       }else{
+           // handle user not found
+           return res.redirect('back');
+       }
+   });
 }
